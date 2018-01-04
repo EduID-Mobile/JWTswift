74097f89313cf8260be87fa8369d9462297f58fc
Julius Saputra <julius_saputra@hotmail.com>
2018-01-04 12:02:00 +0100

    Built Carthage

---
+++ Carthage/Build/iOS/JWTswift.framework/Headers/JWTswift.h
@@ -0,0 +1,16 @@
+//
+//  JWTswift.h
+//  JWTswift
+//
+//  Created by Blended Learning Center on 19.12.17.
+//  Copyright © 2017 Blended Learning Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for JWTswift.
+FOUNDATION_EXPORT double JWTswiftVersionNumber;
+
+//! Project version string for JWTswift.
+FOUNDATION_EXPORT const unsigned char JWTswiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWTswift/PublicHeader.h>
