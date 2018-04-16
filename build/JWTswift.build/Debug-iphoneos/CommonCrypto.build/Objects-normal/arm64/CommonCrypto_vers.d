f65666a129fae8e04ffcba15e3a99af62b149152
Julius Saputra <julius_saputra@hotmail.com>
2018-04-16 13:18:56 +0200

    cleaned derived data

---
+++ build/JWTswift.build/Debug-iphoneos/CommonCrypto.build/Objects-normal/arm64/CommonCrypto_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/blc/Documents/GitHub/JWTswift/build/JWTswift.build/Debug-iphoneos/CommonCrypto.build/DerivedSources/CommonCrypto_vers.c
---
+++ build/JWTswift.build/Debug-iphoneos/JWTswift.build/Objects-normal/arm64/JWTswift_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/blc/Documents/GitHub/JWTswift/build/JWTswift.build/Debug-iphoneos/JWTswift.build/DerivedSources/JWTswift_vers.c
